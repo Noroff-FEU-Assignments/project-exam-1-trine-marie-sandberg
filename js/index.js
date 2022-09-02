9d38f92a9df6fa2e0818bde3c5c48762b8f685ef
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-02 10:56:09 +0200

    created apicall function for posts

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 const postUrl = "http://localhost/mhpb-blogg-content/wordpress-6.0.2/wordpress/wp-json/wp/v2/posts";
+const postContainer = document.querySelector(".post-container");
+postContainer.innerHTML = "Loading . . .";
 
 async function getPosts() {
+
+    try {
+    const response = await fetch(postUrl);
+    const json = await response.json();
+    console.log(json)
+
+    postContainer.innerHTML = "";
+
+    for(let i = 0; i < json.length; i++) {
+        postContainer.innerHTML += `<div>${json[i].content.rendered}<div>`
+    };
     
+    } catch(error) {
+        postContainer.innerHTML = `<div>${error}</div>`
+    };
 }
+
+getPosts()
