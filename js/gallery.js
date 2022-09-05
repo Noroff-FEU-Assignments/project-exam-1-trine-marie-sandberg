a391170d4d8bdfdfc49b012f2e8df68abeb15c22
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-05 17:24:38 +0200

    added images from gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,7 @@
 export function getGallery() {
     const galleryUrl = "http://localhost/mhpb-blogg-content/wordpress-6.0.2/wordpress/wp-json/wp/v2/media";
+    const gallerySlideshowContainer = document.querySelector(".gallery-slideshow-container");
+    gallerySlideshowContainer.innerHTML = "Loading . . .";
 
     async function callGallery() {
 
@@ -9,11 +11,14 @@ export function getGallery() {
             const json = await response.json();
             console.log(json)
 
+            gallerySlideshowContainer.innerHTML = "";
+
             for(let i = 0; i < json.length; i++) {
 
                 if(json[i].alt_text === "gallery") {
 
-                    console.log(json[i].alt_text);
+                    console.log(json[i].source_url);
+                    gallerySlideshowContainer.innerHTML += `<img src="${json[i].source_url}">`;
                 };
             };
 
