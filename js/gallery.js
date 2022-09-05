955e827f74a35603cb8b3d5080f14dd8996b3929
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-05 18:37:36 +0200

    adding buttons to slideshow

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,6 @@
 export function getGallery() {
     const galleryUrl = "http://localhost/mhpb-blogg-content/wordpress-6.0.2/wordpress/wp-json/wp/v2/media";
     const gallerySlideshowContainer = document.querySelector(".gallery-slideshow-container");
-    gallerySlideshowContainer.innerHTML = "Loading . . .";
 
     async function callGallery() {
 
@@ -11,14 +10,13 @@ export function getGallery() {
             const json = await response.json();
             console.log(json)
 
-            gallerySlideshowContainer.innerHTML = "";
-
             for(let i = 0; i < json.length; i++) {
 
                 if(json[i].alt_text === "gallery") {
 
                     console.log(json[i].source_url);
-                    gallerySlideshowContainer.innerHTML += `<img src="${json[i].source_url}" class="auto-img">`;
+                    //gallerySlideshowContainer.innerHTML += `<img src="${json[i].source_url}" class="auto-img">`;
+                    gallerySlideshowContainer.style.backgroundImage = `url("${json[i].source_url}")`;
                 };
             };
 
