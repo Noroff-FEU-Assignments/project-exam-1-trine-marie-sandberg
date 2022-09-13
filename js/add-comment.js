5ef774fbbaa8740a8c69cc10c78161d17ad6b4e3
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-13 12:59:41 +0200

    added single-post.html

--- js/add-comment.js
+++ js/add-comment.js
@@ -8,14 +8,13 @@ export function addComment() {
             let quickPostData = {
                 "author_name": document.querySelector(".quick-post-title").value, 
                 "content": {"rendered": document.querySelector(".quick-post-content").value},
-                "status": "approved"
             }
 
-            let createPost = new XMLHttpRequest();
-            createPost.open("POST", "http://localhost/mhpb-blogg-content/wordpress-6.0.2/wordpress/wp-json/wp/v2/comments");
-            createPost.setRequestHeader("content-type", "application/json;charset=UTF-8");
-            //createPost.setRequestHeader("X-WP-Nonce", userData.nonce)
-            createPost.send(JSON.stringify(quickPostData));
+            let createComment = new XMLHttpRequest();
+            createComment.open("POST", "http://localhost/mhpb-blogg-content/wordpress-6.0.2/wordpress/wp-json/wp/v2/comments");
+            createComment.setRequestHeader("content-type", "application/json;charset=UTF-8");
+            //createComment.setRequestHeader("X-WP-Nonce", userData.nonce)
+            createComment.send(JSON.stringify(quickPostData));
         })
 
         async function displayComments() {
