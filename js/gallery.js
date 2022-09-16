065f64296481fda720ddf56b10c1b52a05251c19
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-16 18:19:10 +0200

    fixing gallery function

--- js/gallery.js
+++ js/gallery.js
@@ -1,83 +1,75 @@
 export function getGallery() {
+    
     const galleryUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/media";
     const gallerySlideshowContainer = document.querySelector(".gallery-slideshow-container");
+    gallerySlideshowContainer.style.backgroundImage = `url("https://gamehub-wp-api.one/mhpb-blogg-content/wp-content/uploads/2022/09/gallery/stone-road.jpg")`;
 
     async function getImages() {
 
         try {
-            let singleImgUrl;
+
             let imageName;
             const response = await fetch(galleryUrl);
             const json = await response.json();
             console.log(json);
             let urlArray = [];
             let matchPattern;
+
             for(let i = 0; i < json.length; i++) {
                 
-                singleImgUrl = json[i].source_url;
-                function searchForGallery(url){
+                let singleImgUrl = json[i].source_url;
+                console.log(json[i].source_url)
+                function searchForGallery(url) {
                     const regExUrl = /gallery/;
                     matchPattern = regExUrl.test(url)
-                    
-                    console.log(regExUrl)
                 
                     if(matchPattern === true) {
                         urlArray.push(url);
-                    }
-                }
+                    };
+                };
+
                 console.log(matchPattern)
-                
                 searchForGallery(singleImgUrl)
-                imageName = json[i].title.rendered;
-
-                
-                console.log(urlArray);
-
+                imageName = json[i].title.rendered;      
             };
-            
-            
-            gallerySlideshowContainer.style.backgroundImage = `url("https://gamehub-wp-api.one/mhpb-blogg-content/wp-content/uploads/2022/09/gallery/stone-road.jpg")`;
 
-            for(let imgIndex = 0; imgIndex < urlArray.length; imgIndex++) {
+            console.log(urlArray);
 
-                function imgSlideshowDisplay() {
-                
-                    gallerySlideshowContainer.style.backgroundImage += `url("${urlArray[imgIndex]}")`;
                     const slideShowNextBtn = document.querySelector(".slideshow-next-btn");
                     const slideShowPreveiousBtn = document.querySelector(".slideshow-previous-btn");
+                    
+                    let imgIndex = 0;
 
                     //NEXT BUTTON
                     slideShowNextBtn.onclick = function nextImg() {
-                        gallerySlideshowContainer.style.backgroundImage += `url("")`;
-                        if(imgIndex >= json.length) {
-                        //console.log("im  too big")
-                        imgIndex = 0;
-                        gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex ++]}")`;
 
-                        } else{
+                        imgIndex ++;
+                        console.log(imgIndex);
+                        if(imgIndex > urlArray.length - 1) {
+                            imgIndex = 0;
+                            console.log("imgIndex > : " + urlArray.length)
+                            gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[0]}")`;
+                        }; 
+
                         
-                        gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex ++]}")`;
-                        };
+                        
+
+                        gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex]}")`;
                     };
                     
                     //PREVIOUS BUTTON
                     slideShowPreveiousBtn.onclick = function preveiousImg() {
-                        gallerySlideshowContainer.style.backgroundImage += `url("")`;
-
-                    if(imgIndex < 1) {
-                        //console.log("im  too small")
-                        imgIndex = json.length - 1;
-                        gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex --]}")`;
-
-                    } else{
-
-                        gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex --]}")`;
-                    };
+                        
+                        if(imgIndex < 1) {
+                            imgIndex = urlArray.length;
+                            console.log("LENGTH: " + urlArray.length)
+                        };
+                        
+                        imgIndex --;
+                        console.log(imgIndex)
+                        
+                        gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex]}")`;
                 };
-            };
-            imgSlideshowDisplay()
-
-        }
 
         } catch(error) {
 
