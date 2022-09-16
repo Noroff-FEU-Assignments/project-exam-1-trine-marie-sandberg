804250f7a66456e9410e843eb645143a91178164
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-16 18:49:54 +0200

    changed name from getGallery to gallerySlideShow

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-export function getGallery() {
+export function gallerySlideShow() {
     
     const galleryUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/media";
     const gallerySlideshowContainer = document.querySelector(".gallery-slideshow-container");
@@ -8,17 +8,16 @@ export function getGallery() {
 
         try {
 
-            let imageName;
+            //let imageName;
             const response = await fetch(galleryUrl);
             const json = await response.json();
-            console.log(json);
             let urlArray = [];
             let matchPattern;
 
             for(let i = 0; i < json.length; i++) {
                 
                 let singleImgUrl = json[i].source_url;
-                console.log(json[i].source_url)
+
                 function searchForGallery(url) {
                     const regExUrl = /gallery/;
                     matchPattern = regExUrl.test(url)
@@ -28,51 +27,40 @@ export function getGallery() {
                     };
                 };
 
-                console.log(matchPattern)
                 searchForGallery(singleImgUrl)
-                imageName = json[i].title.rendered;      
+                //imageName = json[i].title.rendered;      
             };
-
-            console.log(urlArray);
 
                     const slideShowNextBtn = document.querySelector(".slideshow-next-btn");
                     const slideShowPreveiousBtn = document.querySelector(".slideshow-previous-btn");
                     
                     let imgIndex = 0;
 
-                    //NEXT BUTTON
+                    //NEXT btn
                     slideShowNextBtn.onclick = function nextImg() {
 
                         imgIndex ++;
-                        console.log(imgIndex);
-                        if(imgIndex > urlArray.length - 1) {
-                            imgIndex = 0;
-                            console.log("imgIndex > : " + urlArray.length)
-                            gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[0]}")`;
-                        }; 
 
-                        
-                        
+                        if(imgIndex > urlArray.length - 1) {
+
+                            imgIndex = 0;
+                        }; 
 
                         gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex]}")`;
                     };
                     
-                    //PREVIOUS BUTTON
+                    //PREVIOUS btn
                     slideShowPreveiousBtn.onclick = function preveiousImg() {
-                        
-                        //gallerySlideshowContainer.style.backgroundImage += `url("${urlArray[urlArray.length - 1]}")`;
                         
                         if(imgIndex < 1) {
                             
                             imgIndex = urlArray.length - 1;
-                            console.log("LENGTH: " + urlArray.length)
                         };
                         
                         imgIndex --;
-                        console.log(imgIndex)
                         
                         gallerySlideshowContainer.style.backgroundImage = `url("${urlArray[imgIndex]}")`;
-                };
+                    };
 
         } catch(error) {
 
--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import { getPostCards } from "/js/post-cards.js";
-import { getGallery } from "/js/gallery.js";
+import { gallerySlideShow } from "/js/gallery.js";
 import { addComment } from "/js/add-comment.js"
 
 getPostCards();
-getGallery();
+gallerySlideShow();
 addComment();
