aea35b257b4804ce4160916478d758620aecb3b7
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-16 18:56:46 +0200

    namechange gallery to gallery-slide-show

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import { getPostCards } from "/js/post-cards.js";
-import { gallerySlideShow } from "/js/gallery.js";
+import { gallerySlideShow } from "/js/gallery-slide-show.js";
 import { addComment } from "/js/add-comment.js"
 
 getPostCards();
