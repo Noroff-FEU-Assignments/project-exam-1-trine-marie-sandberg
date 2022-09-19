33ed299158fb08be41ef8ace4186b6344cbae6e0
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-19 16:40:05 +0200

    creating post spesific page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 import { getPostCards } from "/js/post-cards.js";
 import { gallerySlideShow } from "/js/gallery-slide-show.js";
 import { addComment } from "/js/add-comment.js";
-import { singlePostPage } from "/js/single-post.js";
+//import { singlePostPage } from "/js/single-post.js";
 
 getPostCards();
 gallerySlideShow();
 addComment();
-singlePostPage();
+//singlePostPage();
--- js/single-post.js
+++ js/single-post.js
@@ -1,15 +1,25 @@
 const queryString = document.location.search;
 const param = new URLSearchParams(queryString);
 const id = param.get("id");
-const postsUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/";
+//const postsUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/";
 const singlePostContainer = document.querySelector(".single-post-container");
-console.log(postsUrl)
+//console.log(postsUrl)
+let singlePostUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/" + id + "?_embed";
 
 async function getSinglePost() {
-    const getPost = await fetch(postsUrl);
-    const postJson = await getPost.json();
+
     
-    for(let i = 0; i < postJson.length; i++) {
-        singlePostContainer.innerHTML = `<div class="single-post-content">${postJson.guid.rendered}</div>`;
-    };
+    const getPost = await fetch(singlePostUrl);
+    const postJson = await getPost.json();
+    console.log(postJson)
+
+    singlePostContainer.innerHTML = `<div class="single-post-content">
+                                     <h2>${postJson.title.rendered}</h2>
+                                     </div>`;
+
+    // for(let i = 0; i < postJson.length; i++) {
+    //     console.log(singlePostUrl)
+    // };
 };
+
+getSinglePost();
