b57d4839f792dc1654d1d7323ecf36b94007bcdb
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-19 14:39:55 +0200

    added footer and singlepost.js

--- js/single-post.js
+++ js/single-post.js
@@ -1,1 +1,15 @@
-export function singlePostPage() {};
+const queryString = document.location.search;
+const param = new URLSearchParams(queryString);
+const id = param.get("id");
+const postsUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/";
+const singlePostContainer = document.querySelector(".single-post-container");
+console.log(postsUrl)
+
+async function getSinglePost() {
+    const getPost = await fetch(postsUrl);
+    const postJson = await getPost.json();
+    
+    for(let i = 0; i < postJson.length; i++) {
+        singlePostContainer.innerHTML = `<div class="single-post-content">${postJson.guid.rendered}</div>`;
+    };
+};
