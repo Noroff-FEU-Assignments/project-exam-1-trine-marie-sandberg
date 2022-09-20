a547751c5ba0d42f311b133e5dbd0c0549580a3f
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-20 12:46:59 +0200

    adjusting single-post pages

--- js/single-post.js
+++ js/single-post.js
@@ -2,7 +2,11 @@ const queryString = document.location.search;
 const param = new URLSearchParams(queryString);
 const id = param.get("id");
 //const postsUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/";
+
 const singlePostContainer = document.querySelector(".single-post-container");
+const aboutAuthorContainer = document.querySelector(".about-author");
+const postContentContainer = document.querySelector(".single-post-content-container");
+
 //console.log(postsUrl)
 let singlePostUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/" + id + "?_embed";
 
@@ -13,17 +17,30 @@ async function getSinglePost() {
         const getPost = await fetch(singlePostUrl);
         const postJson = await getPost.json();
         console.log(postJson)
-        singlePostContainer.innerHTML = `<div class="single-post-content">
+        singlePostContainer.innerHTML = `<div class="single-post-content flex-wrap">
                                      <h2>${postJson.title.rendered}</h2>
                                      <p>${postJson.date}</p>
-                                     <p>${postJson.title.rendered}</p>
-                                     
                                      </div>`;
 
-        if(!postJson._embedded['wp:featuredmedia'][0].source_url) {};
-        if(postJson._embedded['wp:featuredmedia'][0].source_url) {
+        //if(!postJson._embedded['wp:featuredmedia'][0].source_url) {};
+        try {
             singlePostContainer.innerHTML += `<img src="${postJson._embedded['wp:featuredmedia'][0].source_url}" alt="${postJson._embedded['wp:featuredmedia'][0].alt_text}" class="auto-img">`;
+        } catch(error) {
+            console.log(error);
+            singlePostContainer.innerHTML += `<img>`;
         };
+
+        aboutAuthorContainer.innerHTML = `<div class="flex-wrap">
+                                             <h2>Description</h2>
+                                             <div class="author-name-img-wrap">
+                                                 <image src="/img/profile-img.png" class="profile-img">
+                                                 <h2>${postJson._embedded.author[0].name}</h2>
+                                             </div>
+                                          </div>
+                                          <p>Reading time</p>
+                                          <p>${postJson.excerpt.rendered}</p>`;
+
+        postContentContainer.innerHTML = `${postJson.guid.rendered}`
 
     } catch(error) {
         console.log(error)
