3964cbb02a1952f2210f386f0ce7d291d6c09aff
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-21 14:55:29 +0200

    div

--- js/single-post.js
+++ js/single-post.js
@@ -1,13 +1,11 @@
 const queryString = document.location.search;
 const param = new URLSearchParams(queryString);
 const id = param.get("id");
-//const postsUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/";
 
 const singlePostContainer = document.querySelector(".single-post-container");
 const aboutAuthorContainer = document.querySelector(".about-author");
 const postContentContainer = document.querySelector(".single-post-content-container");
 
-//console.log(postsUrl)
 let singlePostUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts/" + id + "?_embed";
 
 async function getSinglePost() {
@@ -42,26 +40,12 @@ async function getSinglePost() {
 
         const removeEmojis = document.querySelector(".booster-reactions-block");
         removeEmojis.style.display ="none";
-        const adjustImages = document.querySelector(`.wp-image-${postJson.featured_media}`);
-        console.log(adjustImages)
-
-        const targetImgContainer = document.querySelector(".wp-block-image");
-        //targetImgContainer.style.margin = "50px 50px 0 0";
-        targetImgContainer.style.display = "flex";
-        //targetImgContainer.style.backgroundColor = "#F9F9F9";
 
         const targetReadtime = document.querySelector(".twp-read-time");
 
     } catch(error) {
         console.log(error)
     }
-    
-
-    
-
-    // for(let i = 0; i < postJson.length; i++) {
-    //     console.log(singlePostUrl)
-    // };
 };
 
 getSinglePost();
