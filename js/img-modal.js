072d1e3d5b53eff40ec5991f159e8bc407f327f4
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-22 20:47:40 +0200

    created modal

--- js/img-modal.js
+++ js/img-modal.js
@@ -1,30 +1,38 @@
 export function imageModal() {
 
     const targetPostImages = document.querySelectorAll("figure > img");
-    
     const imgModalContainer = document.querySelector(".image-modal-container");
 
     targetPostImages.forEach( img => 
         img.addEventListener("click", imgModal));
 
         function imgModal() {
-            imgModalContainer.getElementsByClassName.display="block";
-            console.log("click");
+            
+            imgModalContainer.style.display="block";
 
             try {
 
-                let clickedImage = this
-                console.log(clickedImage)
+                //OPEN MODAL
+                let clickedImage = this;
 
                 let targetPostImagesUrl = clickedImage.getAttribute("src");
-                console.log(targetPostImagesUrl)
+                let targetPostImageAltText = clickedImage.getAttribute("alt");
 
-                imgModalContainer.innerHTML = `<img src="${targetPostImagesUrl}">`;
-                console.log(targetPostImagesUrl)
+                imgModalContainer.innerHTML = `<i class="fa-solid fa-x cansel-x"></i>
+                                               <img src="${targetPostImagesUrl}" class="auto-img modal-img" alt="${targetPostImageAltText}">`;
+
+                //CLOSE MODAL
+                const canselX = document.querySelector(".cansel-x");
+
+                function closeModal() {
+                    
+                    imgModalContainer.style.display = "none";
+                };
+                canselX.addEventListener("click", closeModal);
 
             } catch(error) {
-                console.log(error)
-                imgModalContainer.innerHTML = "cant load img"
-            }
-        }
+                console.log(error);
+                imgModalContainer.innerHTML = "cant load img";
+            };
+        };
 };
