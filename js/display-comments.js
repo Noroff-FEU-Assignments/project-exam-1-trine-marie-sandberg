706be0a481b016e579834cf24aab58fe3059aeec
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-23 14:09:00 +0200

    added condition to only show post spesific comment

--- js/display-comments.js
+++ js/display-comments.js
@@ -1,22 +1,26 @@
-        //DISPLAY COMMENTS
-        const commentsContainer = document.querySelector(".comments-container");
-        export async function displayComments() {
+//Display comments from https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/comments
+const commentsContainer = document.querySelector(".comments-container");
+const queryString = document.location.search;
+const param = new URLSearchParams(queryString);
+const postId = param.get("id");
+export async function displayComments() {
+    
+    try {
+        
+        let getComment = await fetch("https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/comments");
+        let comments = await getComment.json();
+        
+        for(let i = 0; i < comments.length; i++) {
+            console.log(comments[i])
 
-            try {
-
-                let getComment = await fetch("https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/comments");
-                let comments = await getComment.json();
-
-                for(let i = 0; i < comments.length; i++) {
-
-                    //console.log(comments[i])
-                    commentsContainer.innerHTML += `<p>${comments[i].content.rendered}</p>`;
-                }
-
-            } catch(error) {
-
-                console.log(error)
-            }
+            if(comments[i].post == postId) {
+                commentsContainer.innerHTML += `<p>${comments[i].content.rendered}</p>`;
+            };
+            
         };
 
-        displayComments();
+    } catch(error) {
+
+        console.log(error)
+    };
+};
