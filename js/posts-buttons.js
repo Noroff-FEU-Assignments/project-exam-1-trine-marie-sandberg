fe33737ca3a0245f645244bc8560d92d68897673
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-26 20:16:00 +0200

    adjusting post archive

--- js/posts-buttons.js
+++ js/posts-buttons.js
@@ -90,4 +90,38 @@ previousBtn.onclick = function nextPage() {
 
     nextPage()
 };
+
+//SHOW MORE
+const showMoreBtn = document.querySelector(".show-more-btn");
+
+showMoreBtn.onclick = async function showMore() {
+
+    page ++;
+    if(page > 3) {
+        postsContainer.innerHTML += "<h2>You have come to the end of the post archive. New posts are added each week.</h2>";
+    }
+    console.log(page)
+
+    try {
+        
+        let nextUrl = `https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts?page=${page}&_embed`;
+        let nextResponse = await fetch(nextUrl);
+        let nextJson = await nextResponse.json();
+
+        for(let i = 0; i < nextJson.length; i++) {
+            postsContainer.innerHTML += `<a href="single-post.html?id=${nextJson[i].id}">
+            <div class="flex-wrap post-cover">
+                <img src="${nextJson[i]._embedded['wp:featuredmedia'][0].source_url}" alt="${nextJson[i]._embedded['wp:featuredmedia'][0].alt_text}" class="post-cover-img">
+                <div class="post-cover-text">
+                    <h2 class="post-cover-h2">${nextJson[i].title.rendered}</h2>
+                    <p class="post-cover-prev">${nextJson[i].excerpt.rendered}</p>
+                </div>
+            </div>
+         </a>`;
+        };
+
+    } catch(error) {
+        console.log(error);
+    };
+};
 };
