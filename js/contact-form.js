c9832208645dcc675f48833330401e95842d9ab2
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-27 13:31:09 +0200

    styling contact form

---
+++ js/contact-form.js
@@ -0,0 +1,11 @@
+const nameError = document.querySelector(".name-error");
+const emailError = document.querySelector(".email-error");
+const subjectError = document.querySelector(".subject-error");
+const messageError = document.querySelector(".message-error");
+
+const name = document.querySelector(".name");
+const email = document.querySelector(".email");
+const subject = document.querySelector(".subject");
+const message = document.querySelector(".message");
+
+const submitBtn = document.querySelector(".submit-btn");
