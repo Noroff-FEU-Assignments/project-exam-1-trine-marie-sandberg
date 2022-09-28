e115f78ebc38c1a6e8e4f8dbe29ce77e724ad76c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-28 21:23:34 +0200

    added underline to active page

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@ const topNav = document.querySelector(".top-nav");
 
 //OPEN HAMBURGER MENU
 function mobileMenu() {
-    console.log("click")
+
     cancelMenuX.style.display = "block";
     hamburger.style.display = "none";
     topNav.style.display = "block";
@@ -15,9 +15,24 @@ hamburger.addEventListener("click", mobileMenu);
 
 //CLOSE HAMBURGER MENU
 function cancelMenu() {
+
     cancelMenuX.style.display = "none";
     hamburger.style.display = "block";
     topNav.style.display = "none";
 };
 
 cancelMenuX.addEventListener("click", cancelMenu);
+
+//CURRENT PAGE INDICATOR (underline)
+const currentPage = window.location.pathname;
+const navLinks = document.querySelectorAll("nav a");
+const underline = document.querySelector(".nav-underline");
+
+navLinks.forEach(link => {
+
+    if(link.href.includes(`${currentPage}`)) {
+        
+        link.classList.add("nav-underline");
+    };
+    
+});
