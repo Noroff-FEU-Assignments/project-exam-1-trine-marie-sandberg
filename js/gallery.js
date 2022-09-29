4f6b43c70081789f0fe6db20f7015512936f4331
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-29 15:22:31 +0200

    styling gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,12 @@
 import { imageModal } from "/js/img-modal.js";
-const galleryUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/media";
+import { gallerySlideShow } from "/js/gallery-slide-show.js";
+gallerySlideShow()
 
-let miniImages = document.querySelector(".mini-images-container");
+const galleryUrl = "https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/media?per_page=12";
+const miniImages = document.querySelector(".mini-images-container");
 miniImages.innerHTML = "<p>Loading . . .</p>";
-const imgPopupModal = document.querySelector(".image-popup");
 
+//GET IMAGES
 async function getImages() {
     const response = await fetch(galleryUrl);
     const imgJson = await response.json();
@@ -17,9 +19,9 @@ async function getImages() {
             miniImages.innerHTML += `<img class="mini-img" src="${imgJson[i].source_url}">`;
             };
 
+            //IMPORT imageModal
             let targetImages = document.querySelectorAll(".mini-images-container > img");
-            console.log(targetImages)
-        imageModal(targetImages);
+            imageModal(targetImages);
 
     } catch(error) {
 
@@ -28,52 +30,3 @@ async function getImages() {
 };
 
 getImages();
-
-// function imageModal() {
-
-//     const imgPopup = document.querySelector(".image-popup");
-
-//     targetPostImages.forEach( img => 
-//         img.addEventListener("click", imgModal));
-
-//         function imgModal() {
-
-//             imgPopup.style.display="block";
-
-//             try {
-
-//                 //OPEN MODAL
-//                 let clickedImage = this;
-//                 console.log(this)
-//                 console.log(clickedImage)
-
-//                 let targetPostImagesUrl = clickedImage.getAttribute("src");
-//                 let targetPostImageAltText = clickedImage.getAttribute("alt");
-
-//                 imgPopupModal.innerHTML = `<i class="fa-solid fa-x cansel-x"></i>
-//                                                <img src="${targetPostImagesUrl}" class="auto-img modal-img" alt="${targetPostImageAltText}">`;
-
-//                 //CLOSE MODAL (with button)
-//                 const canselX = document.querySelector(".cansel-x");
-
-//                 function closeModal() {
-                    
-//                     imgPopupModal.style.display = "none";
-//                 };
-
-//                 canselX.addEventListener("click", closeModal);
-
-//                 //CLOSE MODAL (click outside of div)
-//                 window.addEventListener('mouseup',function(event){
-
-//                     if(event.target != imgPopupModal && event.target.parentNode != imgPopupModal){
-//                         imgPopupModal.style.display = 'none';
-//                     }
-//               }); 
-
-//             } catch(error) {
-//                 console.log(error);
-//                 imgModalContainer.innerHTML = "cant load img";
-//             };
-//         };                
-// };
