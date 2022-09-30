2e02fa2e16a68db229a1f00b96afb9f5a1d9433e
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-09-30 22:21:36 +0200

    adding search bar

---
+++ js/search.js
@@ -0,0 +1,49 @@
+let searchBar = document.querySelector("#search");
+const allPostsUrl = `https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts?per_page=22`;
+let filterArray = [];
+let postArray = [];
+const searchListContainer = document.querySelector(".search-list-container");
+searchBar.addEventListener("keyup", search);
+
+function search(event) {
+    const searchString = event.target.value;
+    console.log(searchString);
+
+            let filteredPosts = postArray.filter(post => {
+                return post.title.rendered.includes(searchString)
+        });
+        postList(filteredPosts)
+        console.log(filteredPosts)
+};
+
+    async function getPosts() {
+
+    try {
+
+        const getPosts = await fetch(allPostsUrl);
+        postArray = await getPosts.json();
+       
+       
+       console.log(postArray)
+       
+
+
+
+    } catch(error) {
+        console.log(error)
+    }};
+    
+    getPosts();
+
+       const postList = (postArray) => {
+           const htmlString = postArray.map((post) => {
+               return `<a href="">
+                          <li>
+                             <h2>${post.title.rendered}</h2>
+                             <p>${post.slug}</p>
+                          </li>
+                        <a>`;
+           })
+           .join("");
+           searchListContainer.innerHTML = htmlString;
+       };    
