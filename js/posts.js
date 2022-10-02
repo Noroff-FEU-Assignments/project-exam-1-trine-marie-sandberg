5dd0dc859cc7c87e52ab50b3a31c8c26ffb23c6d
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-10-02 23:33:45 +0200

    adjusted text

--- js/posts.js
+++ js/posts.js
@@ -10,6 +10,7 @@ async function getPosts() {
 
     try {
 
+        //GET POSTS
         const postsUrl = `https://gamehub-wp-api.one/mhpb-blogg-content/wp-json/wp/v2/posts?page=1&_embed`;
         const response = await fetch(postsUrl);
         const postJson = await response.json();
@@ -17,12 +18,13 @@ async function getPosts() {
         postsContainer.innerHTML = "";
         for(let i = 0; i < postJson.length; i++) {
 
+            //DISPLAY POSTS
             postsContainer.innerHTML += `<a href="single-post.html?id=${postJson[i].id}">
                                             <div class="flex-wrap post-cover">
                                                 <img src="${postJson[i]._embedded['wp:featuredmedia'][0].source_url}" alt="${postJson[i]._embedded['wp:featuredmedia'][0].alt_text}" class="post-cover-img">
                                                 <div class="post-cover-text">
                                                     <h2 class="post-cover-h2">${postJson[i].title.rendered}</h2>
-                                                    <p class="post-cover-prev">${postJson[i].excerpt.rendered}</p>
+                                                    <p class="post-cover-prev limit-text">${postJson[i].excerpt.rendered}</p>
                                                 </div>
                                             </div>
                                          </a>`;
@@ -33,7 +35,7 @@ async function getPosts() {
         console.log(error);
 
         if(!postsContainer.innerHTML.indexOf("word") != -1) {
-            
+
             postsContainer.innerHTML = `<p>Sorry, could not load content. Please refresh the page, or try again later.</p>
                                         <button class="refresh-btn show-more-btn"><p class="butn-p">REFRESH</p></button>`;
                                         const refreshBtn = document.querySelector(".refresh-btn");
